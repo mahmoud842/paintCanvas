991e83db85584a9e313d9b1a4539d8d807e7222e
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-11-07 23:26:35 +0200

    Updated Inheritence

--- paintCanvas/src/classes.jsx
+++ paintCanvas/src/classes.jsx
@@ -10,9 +10,7 @@ class Shape {
         this.height = 1;
         this.color = 'black';
         this.thickness = 4;
-        console.log("create: " + this.x + " " + this.y + " " + this.width + " " + this.height + " " + this.thickness + " " + this.color);
     }
-
     clear(canva) {
         canva.clearRect(this.x, this.y, this.width, this.height);
     }
@@ -24,17 +22,47 @@ class Shape {
     setThick(thickness) {
         this.thickness = thickness;
     }
+
     draw(canva) {
         throw new Error("The draw method must be implemented in a subclass");
     }
+
     setEndPoint(x, y) {
         this.endx = x;
         this.endy = y;
-        this.x = this.startx;
-        this.y = this.starty;
-        this.width = this.endx - this.startx;
-        this.height = this.endy - this.starty;
+        this.updateSize();
+        this.updatePosition();
         console.log("end: " + this.x + " " + this.y + " " + this.width + " " + this.height + " " + this.thickness + " " + this.color);
+    }
+
+    updateSize() {
+        // Adjust size based on the start and end points
+        this.width = Math.abs(this.endx - this.startx);
+        this.height = Math.abs(this.endy - this.starty);
+    }
+
+    updatePosition() {
+        // Adjust position based on start point (starting from the bottom-left corner)
+        this.x = this.startx;
+        this.y = this.starty - this.height; // Adjust y to make start point the bottom-left corner
+    }
+    clone() {
+        // Create a new instance of the current class
+        const copy = new this.constructor(); // this.constructor ensures the correct class type
+
+        // Copy all properties
+        copy.startx = this.startx;
+        copy.starty = this.starty;
+        copy.endx = this.endx;
+        copy.endy = this.endy;
+        copy.x = this.x;
+        copy.y = this.y;
+        copy.width = this.width;
+        copy.height = this.height;
+        copy.color = this.color;
+        copy.thickness = this.thickness;
+
+        return copy;
     }
 }
 
