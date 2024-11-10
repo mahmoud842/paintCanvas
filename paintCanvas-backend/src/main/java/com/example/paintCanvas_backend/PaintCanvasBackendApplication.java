11f121fa914e634e85ac17427f26e1518dcea04c
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-11-10 08:38:20 +0200

    created the backend project

---
+++ paintCanvas-backend/src/main/java/com/example/paintCanvas_backend/PaintCanvasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.paintCanvas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintCanvasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintCanvasBackendApplication.class, args);
+	}
+
+}
