4b897112661d0312bfc5aab92ba5186169f0fccd
mahmoud842 <mahmoud1hesham2003@gmail.com>
2024-11-30 23:51:07 +0200

    saveLoad

--- paintCanvas/src/SaveAndLoad.jsx
+++ paintCanvas/src/SaveAndLoad.jsx
@@ -5,11 +5,10 @@ class SaveAndLoad {
         this.id = null
     }
 
-    async save(shapes, image, name){
-        const newMssg = new Message("tester name", this.shapes);
-        const url = 'http://localhost:8080/drawings/json';
-        console.log("saving")
-    
+    async saveJsonWithoutID(){
+        const newMssg = new Message("tester name", this.shapes)
+        const url = 'http://localhost:8080/drawings/json'
+        console.log("saving to: " + url)
         try {
             const response = await fetch(url, {
                 method: 'POST',
@@ -32,6 +31,41 @@ class SaveAndLoad {
         } catch (error) {
             console.error('Network error:', error);
         }
+    }
+
+    async saveJsonWithID(){
+        const newMssg = new Message("tester name", this.shapes)
+        const url = 'http://localhost:8080/drawings/' + this.id + '/json'
+        console.log("saving to: " + url)
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: newMssg.name,
+                    shapes: newMssg.shapes
+                }),
+            });    
+            if (response.ok) {
+                const result = await response.json();
+                console.log('Message sent successfully:', result);
+            } else {
+                console.error('Error sending message:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Network error:', error);
+        }
+    }
+
+    async save(shapes, image, name){
+        console.log(shapes)
+        if (this.id == null)
+            this.saveJsonWithoutID()
+        else
+            this.saveJsonWithID()
+
     }
 
     load(){
