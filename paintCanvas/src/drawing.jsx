14143a97a6aeaccb5534277ee4dfd3a66c297a0b
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-11-30 22:40:42 +0200

    Added save to drawing

--- paintCanvas/src/drawing.jsx
+++ paintCanvas/src/drawing.jsx
@@ -1,6 +1,6 @@
 import Shape from './shapes/Shape'
 import ShapeFactory from './shapes/shapeFactory'
-
+import Message from './Message'
 class Drawing {
 
     constructor(){
@@ -234,6 +234,32 @@ class Drawing {
         this.cloneShape = this.selectedShape.clone()
         this.deleteShape()
     }
+    async save() {
+        const newMssg = new Message("tester name", this.shapes);
+        const url = 'http://localhost:8080/drawings/json';
+    
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: newMssg.name,
+                    shapes: newMssg.shapes
+                }),
+            });    
+            if (response.ok) {
+                const result = await response.json();
+                console.log('Message sent successfully:', result);
+            } else {
+                console.error('Error sending message:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Network error:', error);
+        }
+    }
+    
     
 }
 
